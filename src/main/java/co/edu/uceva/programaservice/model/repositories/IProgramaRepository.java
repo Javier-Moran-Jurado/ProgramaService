a7e6d254516896cbcefce1e3d47b132f81b9791e
JAVIER EDUARDO MORAN JUARADO <javier.moran02@uceva.edu.co>
2025-04-06 19:40:21 -0500

    arreglo de archivo duplicado

--- src/main/java/co/edu/uceva/programaservice/model/repositories/IProgramaRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uceva.programaservice.model.repositories;
-
-import co.edu.uceva.programaservice.model.entities.Programa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProgramaRepository extends JpaRepository<Programa, Long> {
-}
